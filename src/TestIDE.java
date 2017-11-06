82645540a08d1f22098320fb535af1c487094b57
August Möller <moau16@nti.umea>
2017-11-06 09:47:07 +0100

    test

---
+++ src/TestIDE.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by August on 2017-11-06.
+ * Created to test IntellJ
+ */
+public class TestIDE {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
